a6086e51754dfca6b772dbb7852bb08f085c8276
seoksoon <seoksun.jang@shopkick.com>
2021-05-25 00:52:45 -0400

    [FIX] swiftfx -> Swiftfx

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftfx",
+    name: "Swiftfx",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v11),
@@ -15,8 +15,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "swiftfx",
-            targets: ["swiftfx"]),
+            name: "Swiftfx",
+            targets: ["Swiftfx"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -26,10 +26,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "swiftfx",
+            name: "Swiftfx",
             dependencies: []),
         .testTarget(
-            name: "swiftfxTests",
-            dependencies: ["swiftfx"]),
+            name: "SwiftfxTests",
+            dependencies: ["Swiftfx"]),
     ]
 )
